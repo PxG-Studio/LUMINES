d694a1383205251897a51ba21c0622dfbed83551
Hiroyasu Mizuha <132397714+HiroyasuDev@users.noreply.github.com>
2025-12-04 09:29:51 -1000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['nats.ws'],
+  },
+  build: {
+    target: 'esnext',
+    commonjsOptions: {
+      include: [/nats\.ws/, /node_modules/],
+    },
   },
 });
