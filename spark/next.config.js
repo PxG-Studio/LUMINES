4c0181b1aed94d321fd60931a693aab647d03118
Hiroyasu Mizuha <132397714+HiroyasuDev@users.noreply.github.com>
2025-12-06 08:24:10 -1000

    Updated next.config.js

--- spark/next.config.js
+++ spark/next.config.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
+    },
+    turbopack: {
+      root: path.resolve(__dirname),
     },
   },
 }
